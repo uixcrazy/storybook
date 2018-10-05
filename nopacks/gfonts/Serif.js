f03a30a6e6eae0c7234e9b10c831da73a174e321
uixcrazy <uixcrazy@gmail.com>
2018-10-05 12:52:12 +0700

    feat (google font): serif

---
+++ nopacks/gfonts/Serif.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { lorem01 } from './lorem';
+import attachRawCss from '../../core/attachRawCss';
+import serif from '!!raw-loader!./serif.css'; // eslint-disable-line
+
+const Serif = () => (
+  <div>
+    <h1>PT Serif</h1>
+    <div className="pt" dangerouslySetInnerHTML={{ __html: lorem01 }} />
+    <h1>Noto Serif</h1>
+    <div className="noto" dangerouslySetInnerHTML={{ __html: lorem01 }} />
+    <h1>Tinos ***</h1>
+    <div className="tinos" dangerouslySetInnerHTML={{ __html: lorem01 }} />
+  </div>
+);
+
+export default attachRawCss(serif, 'serif', Serif);
