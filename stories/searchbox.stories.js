11f0095ace22233bdd093d4e008e3f41067ee476
uixcrazy <uixcrazy@gmail.com>
2018-10-10 13:27:14 +0700

    style (hammerjs): add arrow bottom for activeItem

--- stories/searchbox.stories.js
+++ stories/searchbox.stories.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import SearchBox01 from '../packages/searchbox/src/SearchBox01';
 import SearchBox02 from '../packages/searchbox/src/SearchBox02';
-import SearchBox03 from '../packages/searchbox/src/SearchBox03';
 
 storiesOf('Search Box', module)
   .add('SearchBox01', () => <SearchBox01 />)
-  .add('SearchBox02', () => <SearchBox02 />)
-  .add('SearchBox03', () => <SearchBox03 />);
+  .add('SearchBox02', () => <SearchBox02 />);
